f6dc1c5af8cdf0ea639aac9addb639bc7f7c185e
harshWarbhe <145889549+harshWarbhe@users.noreply.github.com>
2025-03-05 01:01:15 +0530

    Add files via upload

---
+++ CalculateStudentGrade.java
@@ -0,0 +1,31 @@
+package com.java.scerario_based;
+
+public class CalculateStudentGrade {
+	
+	public static StudentGrade calculateGrade(Student student)
+	{
+		int marks = student.getMarks();
+		
+		if(marks > 90)
+		{
+		return new StudentGrade(student, 'A');
+		}
+		else if(marks >+ 75)
+		{
+		return new StudentGrade(student, 'B');
+		}
+		else if(marks >+ 60)
+		{
+		return new StudentGrade(student, 'C');
+		}
+		else
+		{
+			return new StudentGrade(student, 'D');
+		}
+		
+		
+	}
+
+	
+	
+}
---
+++ StudentGradeCalculator.java
@@ -0,0 +1,13 @@
+package com.java.scerario_based;
+
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+	public static void main(String[] args) {
+	
+		
+
+	}
+
+}
